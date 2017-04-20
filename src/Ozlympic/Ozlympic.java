4093b776c39ba33d62b245ebcfb3a52bd694f2b8
Terry413 <s3528894@student.rmit.edu.au>
2017-04-20 15:56:55 +1000

    initialize the project

---
+++ src/Ozlympic/Ozlympic.java
@@ -0,0 +1,13 @@
+package Ozlympic;
+
+public class Ozlympic {
+	/**
+	 * @date 20.04.2017	
+	 * @author Yanjie
+	 * @version 1.0
+	 * @Description main class
+	 */
+    public static void main(String[] args) {
+
+    }
+}
