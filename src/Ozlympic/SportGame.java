89cec17c3bb2c8b35bae1c5370eee25f1e9a78a0
rmit-s3582779-yipeng-zhang <s3582779@student.rmit.edu.au>
2017-05-09 12:09:00 +1000

    change somethine about interface

--- src/Ozlympic/SportGame.java
+++ src/Ozlympic/SportGame.java
@@ -4,10 +4,16 @@ import java.util.ArrayList;
 
 public interface SportGame {
 	public void selectGame(int gameType);
+
 	public ArrayList<String> showGameList();
+
 	public void predict(int newIndex);
-	public void starGame();
+
+	public Boolean starGame();
+
 	public void showFinalResult();
+
 	public void refreshPoint();
+
 	public void exit();
 }
