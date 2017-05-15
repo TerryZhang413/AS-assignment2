f9eb3a88ef10ecabd54f0439d358021ab89c1234
Terry413 <s3528894@student.rmit.edu.au>
2017-05-15 15:43:48 +1000

    add a new stage to implementent the button3 to show all game result

--- src/Ozlympic/MultiWindow.java
+++ src/Ozlympic/MultiWindow.java
@@ -16,6 +16,7 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
@@ -80,6 +81,10 @@ public class MultiWindow extends Application {
 	  button1.setOnAction(gtm);
 	  RunningGame runGame=new RunningGame(driver,games,primaryStage);
 	  button2.setOnAction(runGame);
+	  
+	  ShowFinalResult sfr=new ShowFinalResult(games,driver);
+	  button3.setOnAction(sfr);
+	  
 	  ShowFinalPoint showFinalPoint=new ShowFinalPoint(driver,athletes);
 	  button4.setOnAction(showFinalPoint);
 	//  button4.setOnAction((ActionEvent t)->{driver.showFinalPoint();});
@@ -447,4 +452,70 @@ class ShowResult implements EventHandler<ActionEvent> {
 		showResult.setScene(scene);
 		showResult.show(); // Display the stage
 	}	
+}
+class ShowFinalResult implements EventHandler<ActionEvent> {
+
+	ArrayList<Game> games;
+	Driver driver;
+	
+	ShowFinalResult(ArrayList<Game> games,Driver driver)
+	{
+		this.driver=driver;
+		this.games=games;
+	}
+	
+	@Override
+	public void handle(ActionEvent arg0) {
+		int countGame = games.size();
+		
+		Stage showFinalResult = new Stage();
+		showFinalResult.setTitle("Result"); 
+
+		
+		GridPane  border=new GridPane (); 
+	    ScrollPane scrollPane = new ScrollPane();
+	    scrollPane.setContent(border);
+	    
+		border.setPadding(new Insets(5)); 
+		border.setHgap(20);
+		border.setVgap(5);
+		Scene scene=new Scene(scrollPane,450, 400);         
+		
+		for(int i=0;i<countGame;i++)
+		{
+	        Text GameNumber=new Text( "GameNumber: "+games.get(i).getGameID());
+	        Text OfficalName=new Text( "OfficalName: "+driver.getOffName(MultiWindow.games.get(i).getOfficialID()));
+	        final Text Name=new Text( "Name");
+	        final Text Age=new Text("Age");
+	        final Text State=new Text("State");
+	        final Text AthleteType=new Text("Athlete Type");
+	        final Text Time=new Text("Time");
+	        final Text Point=new Text("Point");
+	        border.add(GameNumber, 0, i*12+0);
+	        border.add(OfficalName, 0, i*12+1);
+	        border.add(Name, 0, i*12+2);
+	        border.add(Age, 1, i*12+2);
+	        border.add(State, 2, i*12+2);
+	        border.add(AthleteType, 3, i*12+2);
+	        border.add(Time, 4, i*12+2);
+	        border.add(Point, 5, i*12+2);
+	
+	        int countAthlete=games.get(i).getAthletes().size();
+	        for(int j=0;j<countAthlete;j++)
+	        {
+	    		String[] athleteinf = driver.getAthleteInf(games.get(i).getAthletes().get(j));
+	        	int time = MultiWindow.games.get(i).getResults().get(j);
+				int point = MultiWindow.games.get(i).getPoints().get(j);
+				border.add(new Text(athleteinf[0]), 0,i*12+j+3);
+				border.add(new Text(athleteinf[1]), 1,i*12+j+3);
+				border.add(new Text(athleteinf[2]), 2,i*12+j+3);
+				border.add(new Text(athleteinf[3]), 3,i*12+j+3);
+				border.add(new Text(time+""), 4,i*12+j+3);
+				border.add(new Text(point+""), 5,i*12+j+3);
+	        }
+        
+		}
+		showFinalResult.setScene(scene);
+		showFinalResult.show(); // Display the stage
+	}
 }
