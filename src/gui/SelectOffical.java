d2ba14394bc4ae44da244e72197bb4a53d5a21b8
Terry413 <s3528894@student.rmit.edu.au>
2017-05-16 19:06:19 +1000

    finish the part to add athletes.need to add offical

---
+++ src/gui/SelectOffical.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class SelectOffical implements EventHandler<ActionEvent>{
+
+	@Override
+	public void handle(ActionEvent arg0) {
+		
+		
+	}
+
+}
