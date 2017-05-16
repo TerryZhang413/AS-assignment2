aa03eb68f0d372e0b4ab416683f82f0312c7062d
Terry413 <s3528894@student.rmit.edu.au>
2017-05-16 17:08:34 +1000

    add a new function to add new athlete

--- src/gui/SelectAthletes.java
+++ src/gui/SelectAthletes.java
@@ -2,6 +2,9 @@ package gui;
 
 import java.util.ArrayList;
 
+import Exception.GameFullException;
+import Exception.NoThisAthleteException;
+import Exception.WrongTypeException;
 import Ozlympic.Athletes;
 import Ozlympic.Driver;
 import javafx.application.Application;
@@ -41,8 +44,15 @@ public class SelectAthletes implements EventHandler<ActionEvent>{
 		driver.setGameType(gameType);
 		closeStage.close();
 		
+		Stage selectedAthlete = new Stage();
+		selectedAthlete.setTitle("Selected Athlete");
+		GridPane pane = new GridPane();
+		pane.setPadding(new Insets(5));
+		pane.setHgap(20);
+		pane.setVgap(5);
+		
 		Stage selectAthlete = new Stage();
-		selectAthlete.setTitle("Final Point");
+		selectAthlete.setTitle("Select Athlete");
 		GridPane border = new GridPane();
 		border.setPadding(new Insets(5));
 		border.setHgap(20);
@@ -52,30 +62,28 @@ public class SelectAthletes implements EventHandler<ActionEvent>{
 		final Text Age = new Text("Age");
 		final Text State = new Text("State");
 		final Text AthleteType = new Text("Athlete Type");
-		final Text Point = new Text("Point");
 		border.add(Name, 1, 0);
 		border.add(Age, 2, 0);
 		border.add(State, 3, 0);
 		border.add(AthleteType, 4, 0);
-		border.add(Point, 5, 0);
 
 		String[] athleteInf = new String[5];
 		int countAthlete = 0;
 		countAthlete = athletes.size();
-		ArrayList<CheckBox> cb=new ArrayList<CheckBox>();
+		ArrayList<Button> choose=new ArrayList<Button>();
 		if (countAthlete == 0) {
 			Stage errorWarning = new Stage();
 			errorWarning.setTitle("Warning");
 			Text warningText = new Text("Thers isn't any athlete's information!");
-			BorderPane pane = new BorderPane();
+			BorderPane warningPane = new BorderPane();
 			pane.setPadding(new Insets(10, 20, 10, 20));
 			String imagePath = this.getClass().getResource("/").getFile();
 			final ImageView warning = new ImageView(new Image(imagePath + "image/warning.png"));
 			Button bt1 = new Button("Ok");
 			Scene sceneWarning = new Scene(pane, 350, 100);
-			pane.setCenter(warningText);
-			pane.setLeft(warning);
-			pane.setBottom(bt1);
+			warningPane.setCenter(warningText);
+			warningPane.setLeft(warning);
+			warningPane.setBottom(bt1);
 			bt1.setOnAction((ActionEvent t) -> {
 				errorWarning.close();
 			});
@@ -88,13 +96,116 @@ public class SelectAthletes implements EventHandler<ActionEvent>{
 				border.add(new Text(athleteInf[1]), 2, i + 1);
 				border.add(new Text(athleteInf[2]), 3, i + 1);
 				border.add(new Text(athleteInf[3]), 4, i + 1);
-				border.add(new Text(athleteInf[4]), 5, i + 1);
-				cb.add(new CheckBox());
-				border.add(cb.get(i), 0, i+1);
+				choose.add(new Button("Choose"));
+				border.add(choose.get(i), 0, i+1);		
+				AddAthlete addAthlete=new AddAthlete(athletes.get(i).getUserID(),driver,pane,athletes);
+				choose.get(i).setOnAction(addAthlete);
+				//border.getChildren().removeAll(choose.get(i));				
 			}
 		}
+		border.add(new Button("Ok"),3,countAthlete+1);		
 		selectAthlete.setScene(scene);
 		selectAthlete.show(); // Display the stage
+		
+		Scene scene1 = new Scene(pane, 400, 550);
+		pane.add(Name, 1, 0);
+		pane.add(Age, 2, 0);
+		pane.add(State, 3, 0);
+		pane.add(AthleteType, 4, 0);	
+		selectedAthlete.setScene(scene1);
+		selectedAthlete.show(); // Display the stage
+		
+	}
+}
+
+class AddAthlete implements EventHandler<ActionEvent>{
+
+	String athelteID;
+	Driver driver;
+	GridPane pane;
+	ArrayList<Athletes> athletes;
+	
+	public AddAthlete(String athelteID,Driver driver, GridPane pane, ArrayList<Athletes> athletes) {
+		this.athelteID=athelteID;
+		this.driver=driver;
+		this.pane=pane;
+		this.athletes=athletes;
 	}
 
+	@Override
+	public void handle(ActionEvent arg0) {
+		try {
+			driver.addAthlete(athelteID);
+			String[] athleteInf = new String[5];
+			athleteInf = driver.getAthleteInf(athelteID);
+			int Ycount=driver.getPresentAthlete().size();
+			pane.add(new Text(athleteInf[0]), 1, Ycount + 1);
+			pane.add(new Text(athleteInf[1]), 2, Ycount + 1);
+			pane.add(new Text(athleteInf[2]), 3, Ycount + 1);
+			pane.add(new Text(athleteInf[3]), 4, Ycount + 1);
+			
+		} catch (WrongTypeException e) {
+	    	Stage errorWarning = new Stage();
+	    	errorWarning.setTitle("Warning");
+	    	Text warningText=new Text("This athlete's type is wrong!!");
+	    	BorderPane pane=new BorderPane();
+	    	pane.setPadding(new Insets(10,20, 10, 20));
+	    	
+			final ImageView warning = new ImageView(
+	      	      new Image("image/warning.png")
+	      	    );
+			Button closeWindow=new Button("Ok");
+			closeWindow.setOnAction((ActionEvent t)->{errorWarning.close();});
+	    	
+	    	Scene sceneWarning = new Scene(pane,350,100);
+	    	pane.setCenter(warningText);
+	    	pane.setLeft(warning);
+	    	pane.setBottom(closeWindow);
+	    	
+	    	errorWarning.setScene(sceneWarning); 
+	    	errorWarning.show();
+		} catch (NoThisAthleteException e) {
+	    	Stage errorWarning = new Stage();
+	    	errorWarning.setTitle("Warning");
+	    	Text warningText=new Text("This athlete is not exist");
+	    	BorderPane pane=new BorderPane();
+	    	pane.setPadding(new Insets(10,20, 10, 20));
+	    	
+			final ImageView warning = new ImageView(
+	      	      new Image("image/warning.png")
+	      	    );
+			Button closeWindow=new Button("Ok");
+			closeWindow.setOnAction((ActionEvent t)->{errorWarning.close();});
+	    	
+	    	Scene sceneWarning = new Scene(pane,350,100);
+	    	pane.setCenter(warningText);
+	    	pane.setLeft(warning);
+	    	pane.setBottom(closeWindow);
+	    	
+	    	errorWarning.setScene(sceneWarning); 
+	    	errorWarning.show();
+		} catch (GameFullException e) {
+	    	Stage errorWarning = new Stage();
+	    	errorWarning.setTitle("Warning");
+	    	Text warningText=new Text("No more than 8 athletes!");
+	    	BorderPane pane=new BorderPane();
+	    	pane.setPadding(new Insets(10,20, 10, 20));
+	    	
+			final ImageView warning = new ImageView(
+	      	      new Image("image/warning.png")
+	      	    );
+			Button closeWindow=new Button("Ok");
+			closeWindow.setOnAction((ActionEvent t)->{errorWarning.close();});
+	    	
+	    	Scene sceneWarning = new Scene(pane,350,100);
+	    	pane.setCenter(warningText);
+	    	pane.setLeft(warning);
+	    	pane.setBottom(closeWindow);
+	    	
+	    	errorWarning.setScene(sceneWarning); 
+	    	errorWarning.show();
+		}
+		
+	}
+	
 }
