e269b5013c8d03a78e5ec2edbe86a29122040fab
rmit-s3582779-yipeng-zhang <s3582779@student.rmit.edu.au>
2017-05-17 11:14:03 +1000

    updata bat

--- Setup.bat
+++ Setup.bat
@@ -1,5 +1,7 @@
 md Ozlympic
-javac -d .\Ozlympic -classpath hsqldb.jar src\Ozlympic\*.java src\Exception\*.java src\Data\*.java src\gui\*.java
+javac -d .\Ozlympic -classpath src\hsqldb.jar src\Ozlympic\*.java src\Exception\*.java src\Data\*.java src\gui\*.java
 xcopy src\image Ozlympic\image\*.* /e
 xcopy src\Data\participants.txt Ozlympic\Data\ /e
+xcopy src\*.jar Ozlympic\*.jar /e
+xcopy src\*.bat Ozlympic\*.bat /e
 pause
